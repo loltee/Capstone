d234930293bb625c2dd05689f862f755c941047f
tracy kendrick <tracykendrick0@gmail.com>
2023-08-28 17:19:41 -0400

    home and index added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,7 +25,58 @@ const Home = () => {
                 </div>
               </div>
             </div>
-            <div className="col-6"></div>
+            <div className="col-6">
+              <div className="d-flex flex-wrap gap-10 justify-content-between align-items-center">
+                <div className="small-banner postion-relative p-3">
+                  <img
+                    src="images/catbanner-01.jpg "
+                    className="img-fluid rounded-3"
+                    alt="main banner"
+                  />
+                  <div className="small-banner-content position-absolute">
+                    <h1> Hot Sale</h1>
+                    <h2>2024 Macbooks</h2>
+                    <p>From $1499.00 or $98.80/mo</p>
+                  </div>
+                </div>
+                <div className="small-banner postion-relative p-3">
+                  <img
+                    src="images/catbanner-02.jpg "
+                    className="img-fluid rounded-3"
+                    alt="main banner"
+                  />
+                  <div className="catbanner-02 position-absolute">
+                    <h1> New New New</h1>
+                    <h2>Tablets</h2>
+                    <p>From $145.00 or $55.50/mo</p>
+                  </div>
+                </div>
+              </div>
+              <div className="small-banner postion-relative p-3">
+                <img
+                  src="images/catbanner-03.jpg "
+                  className="img-fluid rounded-3"
+                  alt="main banner"
+                />
+                <div className="small-banner-content position-absolute">
+                  <h1> Hot Sale</h1>
+                  <h2>2024 Macbooks</h2>
+                  <p>From $1499.00 or $98.80/mo</p>
+                </div>
+                <div className="small-banner postion-relative p-3">
+                  <img
+                    src="images/catbanner-04.jpg "
+                    className="img-fluid rounded-3"
+                    alt="main banner"
+                  />
+                  <div className="small-banner-content position-absolute">
+                    <h1> Hot Sale</h1>
+                    <h2>2024 Macbooks</h2>
+                    <p>From $1499.00 or $98.80/mo</p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </section>
